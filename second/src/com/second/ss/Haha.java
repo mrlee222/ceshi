941f8205692eb884e78d0435e7914db5ccb7db5f
lee <liyonyi330@126.com>
2018-08-08 08:47:51 +0800

    dsssss

--- second/src/com/second/ss/Haha.java
+++ second/src/com/second/ss/Haha.java
@@ -2,7 +2,7 @@ package com.second.ss;
 
 public class Haha {
 	public static void main(String[] args) {
-		System.out.println("00000000000000000000");
+		System.out.println("00000000000000mmmmmmmmmmmmmmm000000");
 	}
 
 }
